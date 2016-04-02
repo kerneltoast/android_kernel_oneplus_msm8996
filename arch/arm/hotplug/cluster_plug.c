6fa30be076d6847025da74c29d814485dd1fecd2
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-04-01 21:45:31 -0400

    cluster_plug: drop the powersuspend bits
    Let's just handle it in the power HAL instead.
    
    Change-Id: I9a05ed9e9ee46d21c5a669426cc8e5ff4df01975

--- arch/arm/hotplug/cluster_plug.c
+++ arch/arm/hotplug/cluster_plug.c
@@ -23,14 +23,6 @@
 #include <linux/input.h>
 #include <linux/cpufreq.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
-
 //#define DEBUG_CLUSTER_PLUG
 #undef DEBUG_CLUSTER_PLUG
 
@@ -58,8 +50,6 @@ static unsigned int sampling_time = DEF_SAMPLING_MS;
 module_param(sampling_time, uint, 0664);
 
 static unsigned int cur_hysteresis = DEF_HYSTERESIS;
-
-static bool suspended = false;
 
 struct cp_cpu_info {
 	u64 prev_cpu_wall;
@@ -142,16 +132,14 @@ static void __ref cluster_plug_work_fn(struct work_struct *work)
 		pr_info("loaded_cpus: %u\n", loaded_cpus);
 #endif
 
-		if (!suspended) {
-			if (loaded_cpus >= 3) {
-				cur_hysteresis = hysteresis;
-				if (online_cpus <= 4)
-					plug_clusters(true);
-			} else if (cur_hysteresis > 0) {
-				cur_hysteresis -= 1;
-			} else {
-				plug_clusters(false);
-			}
+		if (loaded_cpus >= 3) {
+			cur_hysteresis = hysteresis;
+			if (online_cpus <= 4)
+				plug_clusters(true);
+		} else if (cur_hysteresis > 0) {
+			cur_hysteresis -= 1;
+		} else {
+			plug_clusters(false);
 		}
 	}
 
@@ -159,85 +147,11 @@ static void __ref cluster_plug_work_fn(struct work_struct *work)
 		msecs_to_jiffies(sampling_time));
 }
 
-#if defined(CONFIG_POWERSUSPEND) || defined(CONFIG_HAS_EARLYSUSPEND)
-
-#ifdef CONFIG_POWERSUSPEND
-static void __ref cluster_plug_suspend(struct power_suspend *handler)
-#else
-static void __ref cluster_plug_suspend(struct early_suspend *handler)
-#endif
-{
-	if (cluster_plug_active) {
-		int cpu;
-
-		flush_workqueue(clusterplug_wq);
-
-		mutex_lock(&cluster_plug_mutex);
-		suspended = true;
-		mutex_unlock(&cluster_plug_mutex);
-
-		// put rest of the cores to sleep unconditionally!
-		for_each_online_cpu(cpu) {
-			if (cpu != 0)
-				cpu_down(cpu);
-		}
-	}
-}
-
-#ifdef CONFIG_POWERSUSPEND
-static void __ref cluster_plug_resume(struct power_suspend *handler)
-#else
-static void __ref cluster_plug_resume(struct early_suspend *handler)
-#endif
-{
-
-	if (cluster_plug_active) {
-		int cpu;
-
-		mutex_lock(&cluster_plug_mutex);
-		cur_hysteresis = hysteresis;
-		suspended = false;
-		mutex_unlock(&cluster_plug_mutex);
-
-		for_each_possible_cpu(cpu) {
-			if (cpu == 0)
-				continue;
-			cpu_up(cpu);
-		}
-	}
-	queue_delayed_work_on(0, clusterplug_wq, &cluster_plug_work,
-		msecs_to_jiffies(10));
-}
-
-#endif /* CONFIG_POWERSUSPEND) || CONFIG_HAS_EARLYSUSPEND */
-
-#ifdef CONFIG_POWERSUSPEND
-static struct power_suspend cluster_plug_power_suspend_driver = {
-	.suspend = cluster_plug_suspend,
-	.resume = cluster_plug_resume,
-};
-#endif  /* CONFIG_POWERSUSPEND */
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static struct early_suspend cluster_plug_early_suspend_driver = {
-        .level = EARLY_SUSPEND_LEVEL_DISABLE_FB + 10,
-        .suspend = cluster_plug_suspend,
-        .resume = cluster_plug_resume,
-};
-#endif	/* CONFIG_HAS_EARLYSUSPEND */
-
 int __init cluster_plug_init(void)
 {
 	pr_info("cluster_plug: version %d.%d by sultanqasim\n",
 		 CLUSTER_PLUG_MAJOR_VERSION,
 		 CLUSTER_PLUG_MINOR_VERSION);
-
-#ifdef CONFIG_POWERSUSPEND
-	register_power_suspend(&cluster_plug_power_suspend_driver);
-#endif
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	register_early_suspend(&cluster_plug_early_suspend_driver);
-#endif
 
 	clusterplug_wq = alloc_workqueue("clusterplug",
 				WQ_HIGHPRI | WQ_UNBOUND, 1);
