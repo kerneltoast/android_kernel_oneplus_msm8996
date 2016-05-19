b0c718fa5d0e7399f87a20a7556758678f6cecca
Masti, Narayanraddi <c_nmasti@qti.qualcomm.com>
2016-05-19 18:00:34 +0530

    qcacld-2.0: Fix Unitialized heap and stack usage
    1.Fix unitialized heap use in csrGetStatistics by initializing
    pointer of struct type WLANTL_TRANSFER_STA_TYPE to zero.
    2.Fix unitialized stack use in csrRoamReadTSF by initializing
    variable of struct type tCsrNeighborRoamBSSInfo to zero.
    
    Change-Id: I4211b41b5e30d414e45691a5bab4048587cc8499
    CRs-Fixed: 1018486

--- drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiRoam.c
+++ drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiRoam.c
@@ -16680,6 +16680,8 @@ eHalStatus csrGetStatistics(tpAniSirGlobal pMac, eCsrStatsRequesterType requeste
                   pTlStats = (WLANTL_TRANSFER_STA_TYPE *)vos_mem_malloc(sizeof(WLANTL_TRANSFER_STA_TYPE));
                   if (NULL != pTlStats)
                   {
+                     vos_mem_set(pTlStats, sizeof(*pTlStats), 0);
+
                      //req TL for class D stats
                      if(WLANTL_GetStatistics(pMac->roam.gVosContext, pTlStats, staId))
                      {
@@ -16741,6 +16743,8 @@ eHalStatus csrGetStatistics(tpAniSirGlobal pMac, eCsrStatsRequesterType requeste
             pTlStats = (WLANTL_TRANSFER_STA_TYPE *)vos_mem_malloc(sizeof(WLANTL_TRANSFER_STA_TYPE));
             if (NULL != pTlStats)
             {
+               vos_mem_set(pTlStats, sizeof(*pTlStats), 0);
+
                //req TL for class D stats
                if(!VOS_IS_STATUS_SUCCESS(WLANTL_GetStatistics(pMac->roam.gVosContext, pTlStats, staId)))
                {
@@ -18867,10 +18871,11 @@ VOS_STATUS csrRoamReadTSF(tpAniSirGlobal pMac, tANI_U8 *pTimestamp,
                           tANI_U8 sessionId)
 {
     eHalStatus              status     = eHAL_STATUS_SUCCESS;
-    tCsrNeighborRoamBSSInfo handoffNode;
+    tCsrNeighborRoamBSSInfo handoffNode = {{0}};
     tANI_U32                timer_diff = 0;
     tANI_U32                timeStamp[2];
     tpSirBssDescription     pBssDescription = NULL;
+
     csrNeighborRoamGetHandoffAPInfo(pMac, &handoffNode, sessionId);
     pBssDescription = handoffNode.pBssDescription;
     // Get the time diff in milli seconds
