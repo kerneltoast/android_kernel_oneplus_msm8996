91bb4488bfa4092a752e144bcd3abc468060aa63
Sultanxda <sultanxda@gmail.com>
2016-07-15 00:20:05 -0700

    qcom-cpufreq: Increase power cluster max freq to 1401 MHz
    CPU0 is part of the power cluster and it's never unplugged (it's the
    primary CPU), so the extra juice is needed.

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -317,7 +317,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 
 #ifdef CONFIG_MACH_MSM8996_15801
 #define UNDERCLOCKED_MAX_KHZ_PERFCL	1708800
-#define UNDERCLOCKED_MAX_KHZ_PWRCL	1324800
+#define UNDERCLOCKED_MAX_KHZ_PWRCL	1401600
 static bool no_cpu_underclock;
 
 static int __init get_cpu_underclock(char *unused)
