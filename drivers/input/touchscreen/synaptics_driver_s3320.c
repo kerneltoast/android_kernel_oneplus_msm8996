98927bb72c9b4fa54b8f361c12bff3431a76e1b7
Sultanxda <sultanxda@gmail.com>
2016-08-17 11:55:48 -0700

    input: synaptics: s3320: Ensure I2C bus is active inside ISR thread
    When the device is suspended, irq_wake is apparently not enough to ensure
    that the I2C bus is active while an interrupt is processed.
    
    In order to prevent gestures from failing during power collapse, check if
    the I2C bus is active inside the ISR thread. If it isn't, wait until it
    becomes active before processing the interrupt (typically a ~10ms wait).
    
    This fixes off-screen gestures not working sometimes.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -484,6 +484,11 @@ struct synaptics_ts_data {
 	struct work_struct pm_work;
 
 	ktime_t timestamp;
+
+	struct wakeup_source syna_isr_ws;
+	spinlock_t isr_lock;
+	bool i2c_awake;
+	struct completion i2c_resume;
 };
 
 static struct device_attribute attrs_oem[] = {
@@ -1440,6 +1445,8 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 	int ret,status_check;
 	uint8_t status = 0;
 	uint8_t inte = 0;
+	unsigned long flags;
+	bool i2c_active;
 
 	if (atomic_read(&ts->is_stop) == 1)
 	{
@@ -1448,6 +1455,21 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 
 	if( ts->enable_remote) {
 		return IRQ_HANDLED;
+	}
+
+	if (ts->is_suspended) {
+		spin_lock_irqsave(&ts->isr_lock, flags);
+		i2c_active = ts->i2c_awake;
+		spin_unlock_irqrestore(&ts->isr_lock, flags);
+
+		/* I2C bus must be active */
+		if (!i2c_active) {
+			__pm_stay_awake(&ts->syna_isr_ws);
+			/* Wait for I2C to resume before proceeding */
+			reinit_completion(&ts->i2c_resume);
+			wait_for_completion_timeout(&ts->i2c_resume,
+							msecs_to_jiffies(30));
+		}
 	}
 
 	ts->timestamp = ktime_get();
@@ -3830,6 +3852,11 @@ static int synaptics_ts_probe(struct i2c_client *client, const struct i2c_device
 	hrtimer_start(&ts->timer, ktime_set(3, 0), HRTIMER_MODE_REL);
 #endif
 
+	init_completion(&ts->i2c_resume);
+	spin_lock_init(&ts->isr_lock);
+	wakeup_source_init(&ts->syna_isr_ws, "synaptics-isr");
+	ts->i2c_awake = true;
+
 #ifdef TPD_USE_EINT
 	/****************
 	  shoud set the irq GPIO
@@ -4049,6 +4076,11 @@ static int synaptics_i2c_suspend(struct device *dev)
 {
 	int ret;
 	struct synaptics_ts_data *ts = dev_get_drvdata(dev);
+	unsigned long flags;
+
+	spin_lock_irqsave(&ts->isr_lock, flags);
+	ts->i2c_awake = false;
+	spin_unlock_irqrestore(&ts->isr_lock, flags);
 
 	TPD_DEBUG("%s: is called\n", __func__);
 	if (ts->gesture_enable == 1){
@@ -4076,6 +4108,11 @@ static int synaptics_i2c_suspend(struct device *dev)
 static int synaptics_i2c_resume(struct device *dev)
 {
 	struct synaptics_ts_data *ts = dev_get_drvdata(dev);
+	unsigned long flags;
+
+	spin_lock_irqsave(&ts->isr_lock, flags);
+	ts->i2c_awake = true;
+	spin_unlock_irqrestore(&ts->isr_lock, flags);
 
 	TPD_DEBUG("%s is called\n", __func__);
     queue_delayed_work(synaptics_wq,&ts->speed_up_work, msecs_to_jiffies(5));
