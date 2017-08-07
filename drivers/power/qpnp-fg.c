11c66aa53517b3a76052cee13d13b0129e53b586
Sultanxda <sultanxda@gmail.com>
2017-08-07 10:49:42 -0700

    power: qpnp-fg: Move FULL_SOC IRQ enable under use_full_soc_irq
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -7303,9 +7303,9 @@ static int fg_init_irqs(struct fg_chip *chip)
 						chip->soc_irq[FULL_SOC].irq, rc);
 					return rc;
 				}
+				enable_irq_wake(chip->soc_irq[FULL_SOC].irq);
+				chip->full_soc_irq_enabled = true;
 			}
-			enable_irq_wake(chip->soc_irq[FULL_SOC].irq);
-			chip->full_soc_irq_enabled = true;
 
 			if (!chip->use_vbat_low_empty_soc) {
 				rc = devm_request_irq(chip->dev,
