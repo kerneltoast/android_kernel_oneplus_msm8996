04f05e8e9764d88c83d07213e2ec3f1574be9a8f
Sultanxda <sultanxda@gmail.com>
2017-08-09 00:13:25 -0700

    block: Remove iosched switcher
    The major wake lag issues are fixed, and this driver only provides
    negligible benefits, so remove it.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -51,7 +51,6 @@
 #include <linux/async.h>
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
-#include <linux/iosched_switcher.h>
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
@@ -2965,10 +2964,6 @@ static int sd_probe(struct device *dev)
 
 	get_device(&sdkp->dev);	/* prevent release before async_schedule */
 	async_schedule_domain(sd_probe_async, sdkp, &scsi_sd_probe_domain);
-
-	if (!strcmp(sdkp->disk->disk_name, "sda") ||
-		!strcmp(sdkp->disk->disk_name, "sde"))
-		init_iosched_switcher(sdp->request_queue);
 
 	return 0;
 
