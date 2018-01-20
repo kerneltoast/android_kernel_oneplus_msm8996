7be8a2934075f4c3e68935c2d495be6733bb103f
Sultanxda <sultanxda@gmail.com>
2018-01-20 12:20:10 -0800

    qcom-cpufreq: Clean up underclock code
    This is functionally the same as before, but looks much cleaner.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -397,21 +397,6 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 			break;
 		f /= 1000;
 
-#ifdef CONFIG_MACH_MSM8996_15801
-		if (i > 0) {
-			/* Always underclock power cluster for stability */
-			if (cpu < 2) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_pwrcl)
-					break;
-			} else if (!no_cpu_underclock) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_perfcl)
-					break;
-			}
-		}
-#endif
-
 		/*
 		 * Check if this is the last feasible frequency in the table.
 		 *
@@ -432,6 +417,21 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 
 		ftbl[i].driver_data = i;
 		ftbl[i].frequency = f;
+
+#ifdef CONFIG_MACH_MSM8996_15801
+		/* Always underclock power cluster for stability */
+		if (cpu < 2) {
+			if (f == underclk_max_pwrcl) {
+				i++;
+				break;
+			}
+		} else if (!no_cpu_underclock) {
+			if (f == underclk_max_perfcl) {
+				i++;
+				break;
+			}
+		}
+#endif
 	}
 
 	ftbl[i].driver_data = i;
