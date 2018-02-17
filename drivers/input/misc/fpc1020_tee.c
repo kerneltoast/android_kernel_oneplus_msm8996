b0dba4a0c10539093c34446fe3c0fbb33d7c2412
Sultanxda <sultanxda@gmail.com>
2018-02-17 11:23:06 -0800

    misc: fpc1020: Use device struct as wakeup source
    There's no need to create a new wakeup source when every instance of
    struct device already comes with one.
    
    Use the device struct's built-in wakeup source to make things cleaner.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -52,7 +52,6 @@ struct fpc1020_data {
 	struct notifier_block fb_notif;
 	struct completion irq_sent;
 	struct work_struct pm_work;
-	struct wakeup_source ttw_ws;
 	spinlock_t irq_lock;
 
 	bool irq_disabled;
@@ -322,7 +321,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *dev_id)
 	wait_for_completion_timeout(&f->irq_sent, msecs_to_jiffies(100));
 
 	if (f->screen_off)
-		__pm_wakeup_event(&f->ttw_ws, FPC_TTW_HOLD_TIME_MS);
+		pm_wakeup_event(f->dev, FPC_TTW_HOLD_TIME_MS);
 
 	return IRQ_HANDLED;
 }
@@ -367,7 +366,6 @@ static int fpc1020_probe(struct platform_device *pdev)
 	spin_lock_init(&f->irq_lock);
 	INIT_WORK(&f->pm_work, fpc1020_suspend_resume);
 	init_completion(&f->irq_sent);
-	wakeup_source_init(&f->ttw_ws, "fpc_ttw_ws");
 
 	ret = sysfs_create_group(&dev->kobj, &fpc1020_attr_group);
 	if (ret) {
@@ -401,6 +399,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	gpio_direction_input(f->irq_gpio);
 	gpio_direction_output(f->vdd_en_gpio, 1);
+	device_init_wakeup(dev, true);
 
 	return 0;
 err4:
