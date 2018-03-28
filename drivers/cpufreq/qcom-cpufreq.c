ad747a42974431f38c129946073ba6317be472be
Sultanxda <sultanxda@gmail.com>
2018-03-28 13:44:50 -0700

    qcom-cpufreq: Never allow underclock to be disabled
    It's pretty certain that quite a few Snapdragon 820 units are affected by
    the instability, so don't allow them to remove the underclock.
    
    These underclocked frequencies may be adjusted in the future now that
    cpu_stress_test can provide more accurate stability-testing results.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -320,29 +320,14 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 
 #ifdef CONFIG_MACH_MSM8996_15801
 /*
- * Always underclock the power cluster for both MSM8996 and MSM8996pro. There
- * are reproducible crashes with the AnTuTu CPU multithread test when both
- * clusters run at their stock maxfreq. Underclocking the power cluster allows
- * MSM8996 to be stable at its perf cluster's stock maxfreq, and it allows
- * MSM8996pro to be stable at 2150 MHz on its perf cluster.
- *
- * This instability occurs even with a kernel that the OEM compiled.
- * TODO: Investigate why this happens and find a proper fix that allows use of
- * all stock frequencies.
+ * Always underclock both clusters for both MSM8996 and MSM8996pro. There
+ * are reproducible crashes with the cpu_stress_test driver when both clusters
+ * run at their stock maxfreq.
  */
 #define UNDERCLK_MAX_PERFCL_MSM8996PRO	2150400
 #define UNDERCLK_MAX_PWRCL_MSM8996PRO	1516800
 #define UNDERCLK_MAX_PERFCL_MSM8996	1824000
 #define UNDERCLK_MAX_PWRCL_MSM8996	1478400
-static bool no_cpu_underclock;
-
-static int __init get_cpu_underclock(char *unused)
-{
-	no_cpu_underclock = true;
-
-	return 0;
-}
-__setup("no_underclock", get_cpu_underclock);
 #endif
 
 static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
@@ -357,12 +342,6 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 	if (socinfo_get_id() == 305) {
 		underclk_max_perfcl = UNDERCLK_MAX_PERFCL_MSM8996PRO;
 		underclk_max_pwrcl = UNDERCLK_MAX_PWRCL_MSM8996PRO;
-		/*
-		 * TODO: Find out why higher freqs on both clusters crash
-		 * MSM8996pro during AnTuTu's CPU multithread test, even with
-		 * the OEM's kernel.
-		 */
-		no_cpu_underclock = false;
 	} else {
 		underclk_max_perfcl = UNDERCLK_MAX_PERFCL_MSM8996;
 		underclk_max_pwrcl = UNDERCLK_MAX_PWRCL_MSM8996;
@@ -419,13 +398,12 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 		ftbl[i].frequency = f;
 
 #ifdef CONFIG_MACH_MSM8996_15801
-		/* Always underclock power cluster for stability */
 		if (cpu < 2) {
 			if (f == underclk_max_pwrcl) {
 				i++;
 				break;
 			}
-		} else if (!no_cpu_underclock) {
+		} else {
 			if (f == underclk_max_perfcl) {
 				i++;
 				break;
