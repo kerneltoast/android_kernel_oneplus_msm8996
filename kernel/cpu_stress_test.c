21c2a10b76501aa64d968912d146387d337c33e2
Sultanxda <sultanxda@gmail.com>
2018-03-29 13:08:01 -0700

    cpu_stress_test: Improve CPU stress function
    This memcmp is far better at inducing crashes than kfree(NULL) and the
    previous assembly code extracted from kfree(NULL).
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- kernel/cpu_stress_test.c
+++ kernel/cpu_stress_test.c
@@ -24,31 +24,16 @@ static struct workqueue_struct *high_prio_wq;
 
 static void cpu_intensive_func(struct work_struct *work)
 {
-#ifdef CONFIG_ARM64
-	/* Just exchange data between registers and the stack */
+	uint32_t arr1[32] = {0}, arr2[32] = {0};
+
+	/* Just loop a memcmp forever */
 	while (1) {
 		preempt_disable();
-		asm volatile(
-		"stp	x29, x30, [sp, -96]!\n\t"
-		"cmp	x0, 16\n\t"
-		"add	x29, sp, 0\n\t"
-		"stp	x19, x20, [sp,16]\n\t"
-		"stp	x21, x22, [sp,32]\n\t"
-		"stp	x23, x24, [sp,48]\n\t"
-		"stp	x25, x26, [sp,64]\n\t"
-		"str	x27, [sp,80]\n\t"
-		"ldp	x19, x20, [sp,16]\n\t"
-		"ldp	x21, x22, [sp,32]\n\t"
-		"ldp	x23, x24, [sp,48]\n\t"
-		"ldp	x25, x26, [sp,64]\n\t"
-		"ldr	x27, [sp,80]\n\t"
-		"ldp	x29, x30, [sp], 96"
-		);
+		/* This will never break out of the loop */
+		if (memcmp(arr1, arr2, sizeof(arr1)))
+			break;
 		preempt_enable();
 	}
-#else
-	while (1);
-#endif
 }
 
 static int __init cpu_stress_test_init(void)
